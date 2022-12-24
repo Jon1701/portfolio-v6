ef6987b325868a4c4504cb8198baeb696cac6fa9
Jon Balon <development@jonbalon.com>
2022-12-23 19:58:49 -0500

    Index Page Landing Section Component

---
+++ src/components/IndexPage/Sections/Landing/components/ViewMyPortfolioButton.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+
+const StyledButton = styled.button`
+  all: unset;
+
+  border: solid 1px #c4c7c5;
+  padding: 10px;
+
+  user-select: none;
+  cursor: pointer;
+
+  transition: ease-in-out 0.2s;
+
+  &:hover {
+    background-color: #000;
+  }
+`;
+
+const Layout = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+
+  & > *:nth-child(1) {
+    margin-right: 10px;
+  }
+`;
+
+interface Props {
+  /**
+   * Click handler.
+   */
+  handleClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const ViewMyPortfolioButton: React.FC<Props> = ({ handleClick }: Props) => {
+  return (
+    <StyledButton onClick={handleClick}>
+      <Layout>
+        <FontAwesomeIcon icon={faAnglesDown} size="1x" />
+        Click to view my Portfolio
+      </Layout>
+    </StyledButton>
+  );
+};
+
+export default ViewMyPortfolioButton;
---
+++ src/components/IndexPage/Sections/Landing/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import bgURL from './assets/background.jpg';
+import ViewMyPortfolioButton from './components/ViewMyPortfolioButton';
+
+const Container = styled.section`
+  position: relative;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  color: #fff;
+  min-height: 100vh;
+
+  &::before {
+    content: '';
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+
+    background-image: url('${bgURL}');
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+
+    filter: brightness(0.6) grayscale(100%);
+  }
+`;
+
+const Content = styled.div`
+  position: relative;
+
+  max-width: 900px;
+  margin: 0 auto;
+  width: 100%;
+
+  & > *:not(:last-child) {
+    margin-bottom: 25px;
+  }
+`;
+
+const Name = styled.div`
+  font-size: 4rem;
+`;
+
+const Description = styled.div`
+  font-size: 1.5rem;
+  flex-shrink: 2;
+`;
+
+/**
+ * Index Page Landing Section.
+ */
+const LandingSection: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <Name>Jon Balon</Name>
+
+        <Description>
+          <div style={{ width: '60%', margin: '0 auto' }}>
+            I am a Front-End Developer with 5 years of professional experience
+            working with JavaScript, HTML, and CSS in the React ecosystem.
+          </div>
+        </Description>
+
+        <ViewMyPortfolioButton handleClick={() => {}} />
+      </Content>
+    </Container>
+  );
+};
+
+export default LandingSection;
