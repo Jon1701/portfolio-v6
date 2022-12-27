def7b70b2a4a57f87f37887f8188bea507488d7f
Jon Balon <development@jonbalon.com>
2022-12-27 13:09:05 -0500

    Configure `@util` Alias

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -6,6 +6,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
       alias: {
         '@components': path.resolve(__dirname, 'src/components'),
         '@fonts': path.resolve(__dirname, 'src/fonts'),
+        '@util': path.resolve(__dirname, 'src/util'),
       },
     },
   });
