aa9ccf5a450753c9470f904cf25db46170789331
Jon Balon <development@jonbalon.com>
2022-12-30 17:08:46 -0500

    Set Site URL and Favicon in Gatsby Configuration File

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `portfolio-v6`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.jonbalon.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -16,7 +16,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/images/favicon.png',
       },
     },
     'gatsby-plugin-sharp',
