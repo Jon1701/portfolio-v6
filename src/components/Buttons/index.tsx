13ca4b702f7f699306e94a09655cf12cf45d1941
Jon Balon <development@jonbalon.com>
2023-01-07 17:49:34 -0500

    Link Button Component

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -27,6 +27,29 @@ const StyledButton = styled.button`
 `;
 
 /**
+ * Styled anchor.
+ */
+const StyledAnchor = styled.a`
+  all: unset;
+  display: inline-block;
+
+  background-color: #6f1ab6;
+  border: solid 1px #6f1ab6;
+
+  border-radius: 5px;
+  padding: 15px;
+  user-select: none;
+
+  transition: ease-in-out 0.2s;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 0 10px 0 rgb(255 255 255 / 20%);
+    filter: brightness(150%);
+  }
+`;
+
+/**
  * Container for button content.
  */
 const ContainerButtonContent = styled.div`
@@ -95,3 +118,49 @@ export const PrimaryButton: React.FC<ButtonProps> = ({
     </StyledButton>
   );
 };
+
+interface LinkButtonProps {
+  /**
+   * FontAwesome Icon.
+   */
+  icon?: IconProp;
+
+  /**
+   * Icon size.
+   */
+  iconSize?: SizeProp;
+
+  /**
+   * Link destination.
+   */
+  href: string;
+
+  /**
+   * Link content.
+   */
+  children: React.ReactNode;
+}
+
+/**
+ * Link button.
+ */
+export const LinkButton: React.FC<LinkButtonProps> = ({
+  icon,
+  iconSize,
+  href,
+  children,
+}: LinkButtonProps) => (
+  <StyledAnchor href={href} target="_blank">
+    <ContainerButtonContent>
+      {icon !== undefined ? (
+        <FontAwesomeIcon
+          icon={icon}
+          size={iconSize}
+          style={{ marginRight: '10px' }}
+        />
+      ) : null}
+
+      {children}
+    </ContainerButtonContent>
+  </StyledAnchor>
+);
