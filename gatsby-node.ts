001f9df813eb20e512f6695d5592754bfcb5871e
Jon Balon <development@jonbalon.com>
2023-01-11 11:50:17 -0500

    Configure `@assets` Path Alias

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,6 +4,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
+        '@assets': path.resolve(__dirname, 'src/assets'),
         '@components': path.resolve(__dirname, 'src/components'),
         '@fonts': path.resolve(__dirname, 'src/fonts'),
         '@util': path.resolve(__dirname, 'src/util'),
